0b714578121e2f07e63dfe0226953a8f7e3ea0a0
Henry Edmeades <s3606607@student.rmit.edu.au>
2018-04-02 14:13:04 +1000

    FirstMerge

---
+++ RMITHrSystem - Sef Project Assignment 1/src/hrSystem/Abstract.java
@@ -0,0 +1,28 @@
+package hrSystem;
+
+public abstract class Abstract {
+	private String rmitId;
+	private String firstName;
+	private String lastName;
+	
+	public String getRmitId() {
+		return rmitId;
+	}
+	public void setRmitId(String rmitId) {
+		this.rmitId = rmitId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+}
+
---
+++ RMITHrSystem - Sef Project Assignment 1/src/hrSystem/Admin.java
@@ -0,0 +1,10 @@
+package hrSystem;
+
+public class Admin extends CourseCoordinator {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
