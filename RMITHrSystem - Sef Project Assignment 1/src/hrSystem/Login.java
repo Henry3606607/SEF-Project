9ada9e6557616b5f9c83a2cfa21e1e061749471f
s3661022 <36937330+s3661022@users.noreply.github.com>
2018-04-04 18:54:16 +1000

    Update Login.java
    Inserted some input code and scanner input.

--- RMITHrSystem - Sef Project Assignment 1/src/hrSystem/Login.java
+++ RMITHrSystem - Sef Project Assignment 1/src/hrSystem/Login.java
@@ -1,5 +1,23 @@
 package hrSystem;
+import java.util.Scanner;
 
 public class Login {
+  	public static void main(String[] args) {
+      // bit of an idea
+	    Scanner input = new Scanner(System.in);
+
+	    String staffusername;
+	    String staffpassword;
+
+	    System.out.println("Log in:");
+	    System.out.println("username: ");
+	    staffusername = input.next();
+
+	    System.out.println("password: ");
+	    staffpassword = input.next();
+
+	    //checks username and password
+	    if(staffusername.equals(staffusername) && staffpassword.equals(staffpassword)) 
+	        System.out.println("You are logged in");
 
 }
