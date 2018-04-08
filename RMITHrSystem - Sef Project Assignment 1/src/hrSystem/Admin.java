55b119a124487c2fcc56435bbcde4aa909f31b08
s3661022 <36937330+s3661022@users.noreply.github.com>
2018-04-08 19:37:57 +1000

    Update Admin.java
    Made changes with methods

--- RMITHrSystem - Sef Project Assignment 1/src/hrSystem/Admin.java
+++ RMITHrSystem - Sef Project Assignment 1/src/hrSystem/Admin.java
@@ -10,6 +10,69 @@ public class Admin extends User {
 	public Admin(String id, int userID) {
 		this.setRmitId(id);
 		this.setUserID(userID);
+		this.addStaff();
+		this.removeStaff();
 	}
 	
+    public void removeStaff() 
+    {
+    	
+		
+	}
+
+	public void addStaff() 
+	{
+		
+	}
+	
+	public void setRmitId()
+	{
+		
+	}
+	
+	public void setUserID()
+	{
+		
+	}
+	
+	public String getId() 
+	{
+		return id;
+	}
+	
+	public void setId(String id, String setRmitId) 
+	{
+		setRmitId = id;
+	}
+	
+	public String getName() 
+	{
+		return getName();
+	}
+	
+	public void setName(String name, String getName) 
+	{
+		getName = name;
+	}
+	
+	public String getDepartment() 
+	{
+	return getDepartment();
+	}
+	
+	public void setDepartment(String department, String Department) 
+	{
+	Department = department;
+	}
+	
+	public String getSalary() {
+	return getSalary();
+	}
+	
+	public void setSalary(String salary, String Salary) {
+	Salary = salary;
+	}
+	
+
 }
+
